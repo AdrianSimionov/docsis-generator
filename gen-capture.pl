b482f770f18340dea74d307f4b3fc56127410fd3
AdrianSimionov <daniel.simionov@gmail.com>
2015-12-01 15:00:35 +0100

    Added INIT-RNG-REQ and REG-REQ packets. Added Annex C function. Added option to turn on/off clearing of the screen.
    Annex C TLVs is only a placeholder for now.
    
    Annex C TLVs function will allow adding TLVs which might be against MULPI for some packets.
    This is not important as I try to follow the way Wireshark dissects the packet.

--- gen-capture.pl
+++ gen-capture.pl
@@ -8,7 +8,9 @@ use Switch;
 sub SYNC ();
 sub type2UCD();
 sub RNG_REQ();
+sub REG_REQ();
 sub type29UCD();
+sub INIT_RNG_REQ();
 sub B_INIT_RNG_REQ();
 sub type35UCD();
 sub request_minislots();
@@ -26,6 +28,8 @@ my $frame_number = 1;
 my $last_frame = 0;
 my $frame_type;
 
+my $clear_screen = 1;
+
 $pcap_header = "D4C3B2A1020004000000000000000000000004008F000000";
 
 print "\n  Pcap filename to be saved:  ";
@@ -37,25 +41,26 @@ binmode(PCAP_FILE);
 print PCAP_FILE pack "H*", $pcap_header;
 
 while ($last_frame != 1) {
-#  system $^O eq 'MSWin32' ? 'cls' : 'clear';
+  if ($clear_screen) {
+    system $^O eq 'MSWin32' ? 'cls' : 'clear';
+  }
   print "\n  Following packet types can be generated:\n\n";
-  print "   1) SYNC         16)               31)                  46)               61)                             \n";
-  print "   2) Type 2 UCD   17)               32)                  47)                                               \n";
-  print "   3)              18)               33)                  48)                a) Request Frame (minislots)   \n";
-  print "   4) RNG-REQ      19)               34) B-INIT-RNG-REQ   49)                b) Request Frame (bytes)       \n";
-  print "   5)              20)               35) Type 35 UCD      50)                                               \n";
-  print "   6)              21)               36)                  51)                                               \n";
-  print "   7)              22)               37)                  52)                                               \n";
-  print "   8)              23)               38)                  53)                                               \n";
-  print "   9)              24)               39)                  54)                                               \n";
-  print "  10)              25)               40)                  55)                                               \n";
-  print "  11)              26)               41)                  56)                                               \n";
-  print "  12)              27)               42)                  57)                                               \n";
-  print "  13)              28)               43)                  58)                                               \n";
-  print "  14)              29) Type 29 UCD   44)                  59)                                               \n";
-  print "  15)              30)               45)                  60)                                               \n";
+  print "   1) SYNC         16)                31)                  46)               61)                             \n";
+  print "   2) Type 2 UCD   17)                32)                  47)                                               \n";
+  print "   3)              18)                33)                  48)                a) Request Frame (minislots)   \n";
+  print "   4) RNG-REQ      19)                34) B-INIT-RNG-REQ   49)                b) Request Frame (bytes)       \n";
+  print "   5)              20)                35) Type 35 UCD      50)                                               \n";
+  print "   6) REG-REQ      21)                36)                  51)                                               \n";
+  print "   7)              22)                37)                  52)                                               \n";
+  print "   8)              23)                38)                  53)                                               \n";
+  print "   9)              24)                39)                  54)                                               \n";
+  print "  10)              25)                40)                  55)                                               \n";
+  print "  11)              26)                41)                  56)                                               \n";
+  print "  12)              27)                42)                  57)                                               \n";
+  print "  13)              28)                43)                  58)                                               \n";
+  print "  14)              29) Type 29 UCD    44)                  59)                                               \n";
+  print "  15)              30) INIT-RNG-REQ   45)                  60)                                               \n";
   print "\n  Frame " . $frame_number . " - Choose packet type which will be generated:  ";
-
   $frame_type = <>;
   chomp $frame_type;
   switch ($frame_type) {
@@ -68,8 +73,14 @@ while ($last_frame != 1) {
     case 4 {
       ($packet_value, $packet_length) = RNG_REQ();
     }
+    case 6 {
+      ($packet_value, $packet_length) = REG_REQ();
+    }
     case 29 {
       ($packet_value, $packet_length) = type29UCD();
+    }
+    case 30 {
+      ($packet_value, $packet_length) = INIT_RNG_REQ();
     }
     case 34 {
       ($packet_value, $packet_length) = B_INIT_RNG_REQ();
@@ -141,6 +152,9 @@ sub type2UCD() {
   $packet_length = $packet_length + 1;
   # Add TLV data
   while ($last_tlv != 1) {
+    if ($clear_screen) {
+      system $^O eq 'MSWin32' ? 'cls' : 'clear';
+    }
     print "\n  Following TLVs can be added:\n\n";
     print "   1) Modulation Rate\n";
     print "   2) Frequency\n";
@@ -181,6 +195,9 @@ sub type2UCD() {
         $sub_tlv_length = 0;
         $last_sub_tlv = 0;
         while ($last_sub_tlv != 1) {
+          if ($clear_screen) {
+            system $^O eq 'MSWin32' ? 'cls' : 'clear';
+          }
           print "\n  Following sub-TLVs can be added:\n\n";
           print "   1) Modulation Type\n";
           print "   2) Differential Encoding\n";
@@ -258,6 +275,9 @@ sub type2UCD() {
         $sub_tlv_length = 0;
         $last_sub_tlv = 0;
         while ($last_sub_tlv != 1) {
+          if ($clear_screen) {
+            system $^O eq 'MSWin32' ? 'cls' : 'clear';
+          }
           print "\n  Following sub-TLVs can be added:\n\n";
           print "   1) Modulation Type\n";
           print "   2) Differential Encoding\n";
@@ -407,6 +427,21 @@ sub RNG_REQ() {
   return ($packet_value, $packet_length);
 }
 
+sub REG_REQ() {
+  our $packet_value;
+  our $packet_length = 0;
+  # Add Temporary Service Identifier
+  $packet_value = random_bits(16, 0xFFFF);
+  $packet_length = $packet_length + 2;
+  # Add Annex C TLVs
+  ($packet_value, $packet_length) = add_annex_c_tlvs($packet_value, $packet_length);
+  # Add MAC Management header
+  ($packet_value, $packet_length) = add_mac_management($packet_value, $packet_length, "00", "00", "01", "06", "00");
+  # Add DOCSIS header
+  ($packet_value, $packet_length) = add_docsis($packet_value, $packet_length, "0000", undef, "00", 192, 0, 0);
+  return ($packet_value, $packet_length);
+}
+
 sub type29UCD() {
   our $packet_value;
   our $packet_length;
@@ -434,6 +469,9 @@ sub type29UCD() {
   $packet_length = $packet_length + 1;
   # Add TLV data
   while ($last_tlv != 1) {
+    if ($clear_screen) {
+      system $^O eq 'MSWin32' ? 'cls' : 'clear';
+    }
     print "\n  Following TLVs can be added:\n\n";
     print "   1) Modulation Rate\n";
     print "   2) Frequency\n";
@@ -480,6 +518,9 @@ sub type29UCD() {
         $sub_tlv_length = 0;
         $last_sub_tlv = 0;
         while ($last_sub_tlv != 1) {
+          if ($clear_screen) {
+            system $^O eq 'MSWin32' ? 'cls' : 'clear';
+          }
           print "\n  Following sub-TLVs can be added:\n\n";
           print "   1) Modulation Type\n";
           print "   2) Differential Encoding\n";
@@ -662,6 +703,25 @@ sub type29UCD() {
   return ($packet_value, $packet_length);
 }
 
+sub INIT_RNG_REQ() {
+  our $packet_value;
+  our $packet_length = 0;
+  # Add SID
+  $packet_value = random_bits(16, 0x3FFF);
+  $packet_length = $packet_length + 2;
+  # Add Downstream Channel ID
+  $packet_value = $packet_value . random_bits(8, 0xFF);
+  $packet_length = $packet_length + 1;
+  # Add Upstream Channel ID
+  $packet_value = $packet_value . random_bits(8, 0xFF);
+  $packet_length = $packet_length + 1;
+  # Add MAC Management header
+  ($packet_value, $packet_length) = add_mac_management($packet_value, $packet_length, "00", "00", "01", "1E", "00");
+  # Add DOCSIS header
+  ($packet_value, $packet_length) = add_docsis($packet_value, $packet_length, "0000", undef, "00", 192, 0, 0);
+  return ($packet_value, $packet_length);
+}
+
 sub B_INIT_RNG_REQ() {
   our $packet_value;
   our $packet_length = 0;
@@ -711,6 +771,9 @@ sub type35UCD() {
   $packet_length = $packet_length + 1;
   # Add TLV data
   while ($last_tlv != 1) {
+    if ($clear_screen) {
+      system $^O eq 'MSWin32' ? 'cls' : 'clear';
+    }
     print "\n  Following TLVs can be added:\n\n";
     print "   1) Modulation Rate\n";
     print "   2) Frequency\n";
@@ -760,6 +823,9 @@ sub type35UCD() {
         $sub_tlv_length = 0;
         $last_sub_tlv = 0;
         while ($last_sub_tlv != 1) {
+          if ($clear_screen) {
+            system $^O eq 'MSWin32' ? 'cls' : 'clear';
+          }
           print "\n  Following sub-TLVs can be added:\n\n";
           print "   1) Modulation Type\n";
           print "   2) Differential Encoding\n";
@@ -1085,4 +1151,48 @@ sub random_bits {
     $i++;
   }
   return $value;
+}
+
+sub add_annex_c_tlvs {
+  our $packet_value;
+  our $packet_length;
+  our $last_tlv = 0;
+  our $tlv_number = 1;
+  our $tlv_type;
+  $packet_value = $_[0];
+  $packet_length = $_[1];
+  while ($last_tlv != 1) {
+    if ($clear_screen) {
+      system $^O eq 'MSWin32' ? 'cls' : 'clear';
+    }
+    print "\n  Choose Annex C TLVs to be added:\n\n";
+    print "   1) Downstream Frequency\n";
+    print "   2) Upstream Channel ID\n";
+    print "   3) Network Access\n";
+    print "\n  TLV " . $tlv_number . " - Choose which TLV will be generated:  ";
+    $tlv_type = <>;
+    chomp $tlv_type;
+    switch ($tlv_type) {
+      case 1 {
+        $packet_value = $packet_value . "010401020304";
+        $packet_length = $packet_length + 6;
+      }
+      case 2 {
+        $packet_value = $packet_value . "020102";
+        $packet_length = $packet_length + 3;
+      }
+      case 3 {
+        $packet_value = $packet_value . "020103";
+        $packet_length = $packet_length + 3;
+      }
+      else {
+        print "\n  This is not a valid option. Calling EXIT... \n\n";
+        exit;
+      }
+    }
+    print "\n  Is this last TLV for this packet? (Choose: 1 for YES / 0 for NO)  ";
+    $last_tlv = <>;
+    $tlv_number++;
+  }
+  return ($packet_value, $packet_length);
 }
