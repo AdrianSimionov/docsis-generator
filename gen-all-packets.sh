f251977ca0d4858bfa4df20ce5cb59f7c69f49db
Adrian Simionov <daniel.simionov@gmail.com>
2016-11-01 18:56:28 +1100

    SYNC, Type 2 UCD and Request Frames support in automatic mode.
    A loop is also present if a larger number of packets want to be generated.

--- gen-all-packets.sh
+++ gen-all-packets.sh
@@ -5,35 +5,267 @@ set timeout 1
 spawn "./gen-capture.pl"
 
 # Choose filename
-expect ":  " 
+sleep 0.1
+expect ":  "
 send "OK.pcap\r"
 
-# SYNC packet
+for {set i 0} {$i < 1} {incr i 1} {
+
+    # SYNC
+    expect ":  "
+    send "1\r"
+    # Next packet
+    expect ")  "
+    send "0\r"
+
+    # Type 2 UCD
+    expect ":  "
+    send "2\r"
+    # TLV 1
+    expect ":  "
+    send "1\r"
+    # Next TLV
+    expect ")  "
+    send "0\r"
+    # TLV 2
+    expect ":  "
+    send "2\r"
+    # Next TLV
+    expect ")  "
+    send "0\r"
+    # TLV 3
+    expect ":  "
+    send "3\r"
+    # Next TLV
+    expect ")  "
+    send "0\r"
+    # TLV 4
+    expect ":  "
+    send "4\r"
+    # TLV 4.1
+    expect ":  "
+    send "1\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 4.2
+    expect ":  "
+    send "2\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 4.3
+    expect ":  "
+    send "3\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 4.4
+    expect ":  "
+    send "4\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 4.5
+    expect ":  "
+    send "5\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 4.6
+    expect ":  "
+    send "6\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 4.7
+    expect ":  "
+    send "7\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 4.8
+    expect ":  "
+    send "8\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 4.9
+    expect ":  "
+    send "9\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 4.10
+    expect ":  "
+    send "10\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 4.11
+    expect ":  "
+    send "11\r"
+    # Last sub-TLV
+    expect ")  "
+    send "1\r"
+    # Next TLV
+    expect ")  "
+    send "0\r"
+    # TLV 5
+    expect ":  "
+    send "5\r"
+    # TLV 5.1
+    expect ":  "
+    send "1\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 5.2
+    expect ":  "
+    send "2\r"
+
+
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 5.3
+    expect ":  "
+    send "3\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 5.4
+    expect ":  "
+    send "4\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 5.5
+    expect ":  "
+    send "5\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 5.6
+    expect ":  "
+    send "6\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 5.7
+    expect ":  "
+    send "7\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 5.8
+    expect ":  "
+    send "8\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 5.9
+    expect ":  "
+    send "9\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 5.10
+    expect ":  "
+    send "10\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 5.11
+    expect ":  "
+    send "11\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 5.12
+    expect ":  "
+    send "12\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 5.13
+    expect ":  "
+    send "13\r"
+    # Next sub-TLV
+    expect ")  "
+    send "0\r"
+    # TLV 5.14
+    expect ":  "
+    send "14\r"
+    # Last sub-TLV
+    expect ")  "
+    send "1\r"
+    # Next TLV
+    expect ")  "
+    send "0\r"
+    # TLV 6
+    expect ":  "
+    send "6\r"
+    # Next TLV
+    expect ")  "
+    send "0\r"
+    # TLV 7
+    expect ":  "
+    send "7\r"
+    # Next TLV
+    expect ")  "
+    send "0\r"
+    # TLV 15
+    expect ":  "
+    send "15\r"
+    # Next TLV
+    expect ")  "
+    send "0\r"
+    # TLV 16
+    expect ":  "
+    send "16\r"
+    # Next TLV
+    expect ")  "
+    send "0\r"
+    # TLV 18
+    expect ":  "
+    send "18\r"
+    # Next TLV
+    expect ")  "
+    send "0\r"
+    # TLV 19
+    expect ":  "
+    send "19\r"
+    # Last TLV
+    expect ")  "
+    send "1\r"
+
+    # Next packet
+    expect ")  "
+    send "0\r"
+
+    # Request Frame (minislots)
+    expect ":  "
+    send "a\r"
+    # Next packet
+    expect ")  "
+    send "0\r"
+
+    # Request Frame (bytes)
+    expect ":  "
+    send "b\r"
+    # Next packet
+    expect ")  "
+    send "0\r"
+
+}
+
+# Last SYNC packet
 expect ":  "
 send "1\r"
 # Next packet
 expect ")  "
-send "0\r"
-
-# Type 2 UCD
-expect ":  "
-send "2\r"
-# TLV 1
-expect ":  "
 send "1\r"
-# Next TLV
-expect ")  "
-send "0\r"
-# TLV 2
-expect ":  "
-send "2\r"
-# Next TLV
-expect ")  "
-send "1\r"
-
-# Last packet
-expect ")  "
-send "1\r\r"
 
 # print exit code
 expect " "
